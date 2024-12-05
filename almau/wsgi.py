d0d300cc89f83ef40e61f8f6fb376877103a04a7
NortherNomad <kanyshbeisenbek@gmail.com>
2024-12-05 18:41:22 +0000

    little fixes

--- almau/wsgi.py
+++ almau/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+sys.path.append('/var/www/telegramBot')
 
 from django.core.wsgi import get_wsgi_application
 
